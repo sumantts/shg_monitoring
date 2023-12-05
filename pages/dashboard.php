b559452fa889fdad5866fb3d352a99bbc0c9e993
suman <>
2023-12-05 10:03:35 +0530

    dashboard function done

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -4,8 +4,42 @@
 	$StfId = $_SESSION["StfId"];
 	$StfNm = $_SESSION["StfNm"];
 	$CollDt = date('m/d/Y');
-	
+	$all_results = array();
+	$grantAmtColl = 0;
+
+	//Get Group Members
+	$query2 = "CALL usp_ViewDashboard('".$StfId."')";
+	mysqli_multi_query($con, $query2);
+	do {
+		/* store the result set in PHP */
+		if ($result2 = mysqli_store_result($con)) {
+			while ($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
+				//printf("%s\n", $row[0]);
+				$Dt = $row2['Dt'];
+				$GrpNo = $row2['GrpNo'];
+				$AmtColl = $row2['AmtColl'];
+				
+				$grantAmtColl = $grantAmtColl + $AmtColl;
+
+				if($Dt != ''){
+					$all_result = new stdClass();
+					
+					$all_result->Dt = $Dt; 
+					$all_result->GrpNo = $GrpNo;
+					$all_result->AmtColl = $AmtColl;
+
+					array_push($all_results, $all_result);
+				}
+			}
+		}
+		/* print divider */
+		if (mysqli_more_results($con)) {
+			//printf("-----------------\n");
+		}
+	} while (mysqli_next_result($con));
+	/* execute multi query */
 		
+	//echo json_encode($all_results);
 ?>
   <body>
     <div class="container-scroller">
@@ -20,8 +54,8 @@
 			<div class="col-lg-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <h4 class="card-title">Collection Summary of <?=$StfNm?></h4>
-                    <p class="card-description"> Welcome to Mahila Bikash dashboard </p>
+                    <h4 class="card-description"> Welcome to Mahila Bikash dashboard </h4>
+                    <p class="card-title mt-5">Collection Summary of <?=$StfNm?></p>
                     <div class="table-responsive-sm">
 						<table class="table table-bordered">
 						<thead>
@@ -33,30 +67,30 @@
 							</tr>
 						</thead>
 						<tbody>
+							<?php
+							if(sizeof($all_results) > 0){
+								for($i = 0; $i < sizeof($all_results); $i++){
+							?>
 							<tr>
-							<td style="text-align: center;">22-11-2023</td>
-							<td style="text-align: right;"> </td>
-							<td style="text-align: right;"> </td>
-							<td style="text-align: center;"><a href="?p=dashboard-more">Show</a></td>
+							<td style="text-align: center;"><?=$all_results[$i]->Dt?></td>
+							<td style="text-align: right;"><?=$all_results[$i]->GrpNo?></td>
+							<td style="text-align: right;"><?=number_format($all_results[$i]->AmtColl, 2)?></td>
+							<td style="text-align: center;"><a href="?p=dashboard-more&dt=<?=$all_results[$i]->Dt?>">Show</a></td>
 							</tr>
-							<tr>
-							<td style="text-align: center;">21-11-2023</td>
-							<td style="text-align: right;"> </td>
-							<td style="text-align: right;"> </td>
-							<td style="text-align: center;"><a href="?p=dashboard-more">Show</a></td>
-							</tr>
-							<tr>
-							<td style="text-align: center;">20-11-2023</td>
-							<td style="text-align: right;"> </td>
-							<td style="text-align: right;"> </td>
-							<td style="text-align: center;"><a href="?p=dashboard-more">Show</a></td>
-							</tr>
+							<?php } ?>
 							<tr>
 							<td style="text-align: center;">Total</td>
 							<td></td> 
-							<td></td> 
-							<td style="text-align: right;"> </td>
+							<td style="text-align: right;"><?=number_format($grantAmtColl, 2)?></td> 
+							<td style="text-align: right;"></td>
 							</tr>
+							<?php }else{ ?>
+							<tr>
+							<td style="text-align: center;" colspan="4">Sorry! No data Found</td>
+							</tr>
+							<?php } ?>
+							
+							
 						</tbody>
 						</table>
 					</div>
--- pages/dashboard_more.php
+++ pages/dashboard_more.php
@@ -3,7 +3,50 @@
 	include('common/header.php');
 	$StfId = $_SESSION["StfId"];
 	$StfNm = $_SESSION["StfNm"];
-	$CollDt = date('m/d/Y');
+	
+
+	$dt = $_GET['dt'];
+	$all_results = array();
+	$grantAmtColl = 0;
+
+
+	if(isset($_GET['dt'])){
+		$dt = $_GET['dt'];
+
+		$query2 = "CALL usp_ViewDashboardDtls('".$StfId."', '".$dt."')";
+		mysqli_multi_query($con, $query2);
+		do {
+			/* store the result set in PHP */
+			if ($result2 = mysqli_store_result($con)) {
+				while ($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
+					//printf("%s\n", $row[0]);
+					$Dt = $row2['Dt'];
+					$GrpNo = $row2['GrpNo'];
+					$AmtColl = $row2['AmtColl'];
+					
+					$grantAmtColl = $grantAmtColl + $AmtColl;
+
+					if($Dt != ''){
+						$all_result = new stdClass();
+						
+						$all_result->Dt = $Dt; 
+						$all_result->GrpNo = $GrpNo;
+						$all_result->AmtColl = $AmtColl;
+
+						array_push($all_results, $all_result);
+					}
+				}
+			}
+			/* print divider */
+			if (mysqli_more_results($con)) {
+				//printf("-----------------\n");
+			}
+		} while (mysqli_next_result($con));
+		/* execute multi query */
+	}//end if
+		
+	//echo json_encode($all_results);
+
 	
 		
 ?>
@@ -21,7 +64,7 @@
                 <div class="card">
                   <div class="card-body">
                     <h4 class="card-title">Collection Summary of <?=$StfNm?></h4>
-                    <p class="card-description"> Collection Data On 22-11-2023</p>
+                    <p class="card-description"> Collection Data On <?=$dt?></p>
                     <div class="table-responsive-sm">
 						<table class="table table-bordered">
 						<thead>
